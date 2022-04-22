774bfd5cf763f5107038dcaeb72eb8ee6a1a8c25
Jessica D <jdahl08@gmail.com>
2022-04-22 12:40:43 -0400

    first commit, random quotes included

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+let quotes = [
+    {quote: 'The journey of a thousand miles begins with one step', source: 'Lao Tzu', citation: 'Chinese proverb' },
+    {quote: 'Life is what happens when you\'re busy making other plans', source: 'John Lennon', citation: 'Song: Beautiful Boy', year: 1980 },
+    {quote: 'Get busy living or get busy dying...there ain\'t nothing in between', source: 'Stephen King', citation: 'Different Seasons', year: 1982 },
+    {quote: 'Life is like a box of chocolates. You never know what you\'re going to get', source: 'Forrest Gump', citation: 'Movie: Forrest Gump', year: 1994 }
+  ]
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
